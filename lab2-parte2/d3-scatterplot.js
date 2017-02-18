060888132479db49d765fa3c51ffc211561ee52d
Órion Winter <orion.lima@ccc.ufcg.edu.br>
2017-02-18 07:14:30 -0300

    Adicionando mudanças do lab 2

--- lab2-parte2/d3-scatterplot.js
+++ lab2-parte2/d3-scatterplot.js
@@ -2,30 +2,6 @@
  * Created by orion on 21/12/16.
  */
 
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-var x = d3.scale.linear()
-    .range([0, width]);
-
-var y = d3.scale.linear()
-    .range([height, 0]);
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
 var data = [
     {
         "diferenca_tempo": 88,
@@ -1268,6 +1244,30 @@ var data = [
         "sd_regioes": 26.787745954696
     }
 ];
+
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var x = d3.scale.linear()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 data.forEach(function(d) {
     d.diferenca_tempo = +d.diferenca_tempo;
